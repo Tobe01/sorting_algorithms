f0d78bc9baa14f2d9a7a498a9af30ec03587c44e
Tobechi Duru <tobechi.duru001@gmail.com>
2024-01-22 13:53:39 -0800

    Updates

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,15 +9,6 @@
  * @a: The first integer to swap.
  * @b: The second integer to swap.
 */
-void swap_ints(int *a, int *b)
-{ 
-	int tmp;
-
-	tmp = *a;
-	*a = *b;
-	*b = tmp;
-}
-
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, len = size;
